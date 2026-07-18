4003acefb60bb299525a86671365ae8b631b619b
aryanmishra55254 <aryanmishra55254@gmail.com>
2026-07-18 20:49:46 +0530

    PR webhook test

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ A distributed, event-driven CI engine that automates code builds inside isolated
 # Architecture Diagram 
 
 ![ARCHITECTURE](public/image.png)test
+pr test
--- apps/api_gateway/src/index.ts
+++ apps/api_gateway/src/index.ts
@@ -20,181 +20,182 @@ const fastify = Fastify({
     logger: true,
 });
 
-// general health check for the api 
-fastify.get("/health", async (_, reply) => {
-    return reply
-        .code(200)
-        .type("text/plain")
-        .send("Hello, Kiln Is Alive");
-});
-
-// main GH webhook endpoint Every Push/Pull Request enters Kiln through this route.
-fastify.post("/webhooks/github", { config: { rawBody: true, }, }, async (request, reply) => {
-
-    //extract signature
-    const rawSignature = request.headers['x-hub-signature-256'];
-
-    const githubSignature: string = Array.isArray(rawSignature)
-        ? rawSignature[0]
-        : (rawSignature || '');
-
-    if (!githubSignature) {
-        return reply
-            .code(401)
-            .send({ error: "Missing signature" });
-    }
-
-    const rawBody = request.rawBody;
-
-    if (!rawBody) {
-        return reply
-            .code(400)
-            .send({ error: "Missing raw body" });
-    }
-
-    // Verify Signature
-    const verifier = process.env.GITHUB_VERIFIER;
-
-    if (!verifier) {
-        throw new Error("GITHUB_VERIFIER is not configured");
-    }
-
-    const expectedSignature =
-        "sha256=" + createHmac("sha256", verifier)
-            .update(rawBody)
-            .digest("hex");
-
-    const githubSignatureBuffer = Buffer.from(githubSignature, 'utf-8');
-    const trustedBuffer = Buffer.from(expectedSignature, 'utf-8');
-
-    if (trustedBuffer.length !== githubSignatureBuffer.length) {
-        return reply.code(401).send({ error: 'Unauthorized' });
-    }
-
-    if (!timingSafeEqual(trustedBuffer, githubSignatureBuffer)) {
-        return reply.code(401).send({ error: 'Unauthorized' });
-    }
-
-    // check if delivery if is new and unique and don't create duplicate CI
-    const rawDeliveryID = request.headers["x-github-delivery"];
-
-    const deliveryId = Array.isArray(rawDeliveryID)
-        ? rawDeliveryID[0]
-        : rawDeliveryID;
-
-    if (!deliveryId) {
-        return reply
-            .code(400)
-            .send({ error: "Missing GitHub delivery ID" });
-    }
-
-    const existingDelivery = await db.query(
-        `
-    SELECT delivery_id
-    FROM webhook_deliveries
-    WHERE delivery_id = $1
-    `,
-        [deliveryId]
-    );
-
-    if (existingDelivery.rowCount !== 0) {
-        return reply.code(200).send({
-            success: true,
-            duplicate: true,
-        });
-    }
-
-    // check if events are PR or pushed , don't support anything else 
-    const rawEvent = request.headers["x-github-event"];
-
-    const event = Array.isArray(rawEvent)
-        ? rawEvent[0]
-        : rawEvent;
-
-    if (!event) {
-        return reply.code(400).send({
-            error: "Missing GitHub event type",
-        });
-    }
-
-    if (event !== "push" && event !== "pull_request") {
-        return reply.code(200).send({
-            success: true,
-            ignored: true,
-        });
-
-    }
-
-    //normalizing payloads
-    let normalizedPayload: normalized_payload;
-
-    if (event === "push") {
-        const body = request.body as Push;
-
-        normalizedPayload = {
-            eventType: "push",
-            repoName: body.repository.name,
-            repoOwner: body.repository.owner.login,
-            branch: body.ref.replace("refs/heads/", ""),
-            commitSha: body.after,
-        };
-    } else {
-        const body = request.body as PR;
-
-        normalizedPayload = {
-            eventType: "pull_request",
-            repoName: body.repository.name,
-            repoOwner: body.repository.owner.login,
-            branch: body.pull_request.head.ref,
-            commitSha: body.pull_request.head.sha,
-        };
-    }
-
-    //persist ci job
-    const client = await db.connect();
-
-    try {
-        const job_uuid = randomUUID();
-        const jobqueryText = `INSERT INTO jobs (id , repo_owner , repo_name , branch , commit_sha , event_type , status)
-        VALUES($1, $2, $3 , $4 , $5 , $6 , 'awaiting_source')` ;
-        const jobvalues = [job_uuid, normalizedPayload.repoOwner, normalizedPayload.repoName, normalizedPayload.branch, normalizedPayload.commitSha, normalizedPayload.eventType];
-
-        const deliveryqueryText = `INSERT INTO webhook_deliveries (delivery_id , job_id) VALUES($1 , $2)`
-        const deliveryvalues = [deliveryId, job_uuid];
-
-
-        await client.query('BEGIN');
-        await client.query(jobqueryText, jobvalues);
-        await client.query(deliveryqueryText, deliveryvalues);
-        await client.query('COMMIT');
-        console.log('Transaction successful! All data saved.');
-
-    } catch (error) {
-        await client.query('ROLLBACK');
-        console.error('Transaction failed. Database rolled back.');
-        throw error;
-    }
-    finally {
-        //releasing pg connection back to the pool always
-        client.release();
-    }
-
-    return reply.code(200).send("Job Persisted");
-})
-
 // start the api gateway 
 const start = async () => {
     try {
 
         await fastify.register(fastifyRawBody, {
             field: "rawBody",
-            global: false,
+            global: true,
             encoding: false,
             runFirst: true,
         });
 
         await testConnection(); // db connection
         await initDatabase();   // db initiazation
+
+
+        // general health check for the api 
+        fastify.get("/health", async (_, reply) => {
+            return reply
+                .code(200)
+                .type("text/plain")
+                .send("Hello, Kiln Is Alive");
+        });
+
+        // main GH webhook endpoint Every Push/Pull Request enters Kiln through this route.
+        fastify.post("/webhooks/github", { config: { rawBody: true, }, }, async (request, reply) => {
+
+            //extract signature
+            const rawSignature = request.headers['x-hub-signature-256'];
+
+            const githubSignature: string = Array.isArray(rawSignature)
+                ? rawSignature[0]
+                : (rawSignature || '');
+
+            if (!githubSignature) {
+                return reply
+                    .code(401)
+                    .send({ error: "Missing signature" });
+            }
+
+            const rawBody = request.rawBody;
+
+            if (!rawBody) {
+                return reply
+                    .code(400)
+                    .send({ error: "Missing raw body" });
+            }
+
+            // Verify Signature
+            const verifier = process.env.GITHUB_VERIFIER;
+
+            if (!verifier) {
+                throw new Error("GITHUB_VERIFIER is not configured");
+            }
+
+            const expectedSignature =
+                "sha256=" + createHmac("sha256", verifier)
+                    .update(rawBody)
+                    .digest("hex");
+
+            const githubSignatureBuffer = Buffer.from(githubSignature, 'utf-8');
+            const trustedBuffer = Buffer.from(expectedSignature, 'utf-8');
+
+            if (trustedBuffer.length !== githubSignatureBuffer.length) {
+                return reply.code(401).send({ error: 'Unauthorized' });
+            }
+
+            if (!timingSafeEqual(trustedBuffer, githubSignatureBuffer)) {
+                return reply.code(401).send({ error: 'Unauthorized' });
+            }
+
+            // check if delivery if is new and unique and don't create duplicate CI
+            const rawDeliveryID = request.headers["x-github-delivery"];
+
+            const deliveryId = Array.isArray(rawDeliveryID)
+                ? rawDeliveryID[0]
+                : rawDeliveryID;
+
+            if (!deliveryId) {
+                return reply
+                    .code(400)
+                    .send({ error: "Missing GitHub delivery ID" });
+            }
+
+            const existingDelivery = await db.query(
+                `
+    SELECT delivery_id
+    FROM webhook_deliveries
+    WHERE delivery_id = $1
+    `,
+                [deliveryId]
+            );
+
+            if (existingDelivery.rowCount !== 0) {
+                return reply.code(200).send({
+                    success: true,
+                    duplicate: true,
+                });
+            }
+
+            // check if events are PR or pushed , don't support anything else 
+            const rawEvent = request.headers["x-github-event"];
+
+            const event = Array.isArray(rawEvent)
+                ? rawEvent[0]
+                : rawEvent;
+
+            if (!event) {
+                return reply.code(400).send({
+                    error: "Missing GitHub event type",
+                });
+            }
+
+            if (event !== "push" && event !== "pull_request") {
+                return reply.code(200).send({
+                    success: true,
+                    ignored: true,
+                });
+
+            }
+
+            //normalizing payloads
+            let normalizedPayload: normalized_payload;
+
+            if (event === "push") {
+                const body = request.body as Push;
+
+                normalizedPayload = {
+                    eventType: "push",
+                    repoName: body.repository.name,
+                    repoOwner: body.repository.owner.login,
+                    branch: body.ref.replace("refs/heads/", ""),
+                    commitSha: body.after,
+                };
+            } else {
+                const body = request.body as PR;
+
+                normalizedPayload = {
+                    eventType: "pull_request",
+                    repoName: body.repository.name,
+                    repoOwner: body.repository.owner.login,
+                    branch: body.pull_request.head.ref,
+                    commitSha: body.pull_request.head.sha,
+                };
+            }
+
+            //persist ci job
+            const client = await db.connect();
+
+            try {
+                const job_uuid = randomUUID();
+                const jobqueryText = `INSERT INTO jobs (id , repo_owner , repo_name , branch , commit_sha , event_type , status)
+        VALUES($1, $2, $3 , $4 , $5 , $6 , 'awaiting_source')` ;
+                const jobvalues = [job_uuid, normalizedPayload.repoOwner, normalizedPayload.repoName, normalizedPayload.branch, normalizedPayload.commitSha, normalizedPayload.eventType];
+
+                const deliveryqueryText = `INSERT INTO webhook_deliveries (delivery_id , job_id) VALUES($1 , $2)`
+                const deliveryvalues = [deliveryId, job_uuid];
+
+
+                await client.query('BEGIN');
+                await client.query(jobqueryText, jobvalues);
+                await client.query(deliveryqueryText, deliveryvalues);
+                await client.query('COMMIT');
+                console.log('Transaction successful! All data saved.');
+
+            } catch (error) {
+                await client.query('ROLLBACK');
+                console.error('Transaction failed. Database rolled back.');
+                throw error;
+            }
+            finally {
+                //releasing pg connection back to the pool always
+                client.release();
+            }
+
+            return reply.code(200).send("Job Persisted");
+        })
 
         // server start
 
@@ -208,5 +209,7 @@ const start = async () => {
         process.exit(1);
     }
 };
+
+
 
 start();
